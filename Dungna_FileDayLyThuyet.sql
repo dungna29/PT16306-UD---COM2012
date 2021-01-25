7042de5b8a443da20e37f9e5de72a37c7630c83a
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2021-01-25 13:51:22 +0700

    Add files via upload

---
+++ Dungna_FileDayLyThuyet.sql
@@ -0,0 +1,87 @@
+﻿/* T-SQL - Structured Query Language: Ngôn ngữ truy vấn có cấu trúc SQL cho phép Tạo CSDL, Thao tác trên dữ liệu
+		(Lưu trữ dữ liệu, Sửa dữ liệu, Xóa dữ liệu) Đa số các DBMS hiện nay sử dụng SQL (MS SQL Server – T- SQL, 
+		Microsoft Access, Oracle – PL/SQL, DB2, MySQL…) [DungNA29]
+Có thể chia thành 4 nhóm lệnh SQL:
+	+ Nhóm truy vấn dữ liệu (DQL): gồm các lệnh truy vấn lựa chọn
+	(Select) để lấy thông tin nhưng không làm thay đổi dữ liệu trong các bảng
+	+ Nhóm định nghĩa dữ liệu (DDL): Gồm các lệnh tạo, thay đổi các bảng dữ liệu(Create, Drop, Alter, …)
+	+ Nhóm thao tác dữ liệu (DML): Gồm các lệnh làm thay đổi dữ liệu (Insert, Delete, Update,…) lưu trong các bảng
+	+ Nhóm điều khiển dữ liệu (DCL): Gồm các lệnh quản lý quyền truy nhập vào dữ liệu và các bảng (Grant, Revoke, …)
+SQL KHÔNG PHÂN BIỆT CHỮA HOA CHỮ THƯỜNG. [DungNA29]
+NGUYÊN TẮC KHI ĐẶT TÊN
+	+ Kí tự đầu tiên của một định danh phải là một kí tự chữ cái theo chuẩn Unicode 2.0, hoặc dấu (_),
+	hoặc dấu @ (tên biến), hoặc # (bảng tạm).
+    + Không trùng với các từ khoá và từ dành riêng của ngôn ngữ T-SQL
+    + Không chứa các kí tự đặt biệt +, -, *, /, !, ~, | ....
+    + Ví dụ tên hợp lệ: Nhan_vien, _PhongBan
+    + Tên không hợp lệ: [%], SELECT
+    [DungNA29]
+	Ctrl + E = EExxcute câu lệnh khi bôi đen vùng lệnh.
+	*/
+
+-- Câu lệnh 1.1: COMMENT TRONG SQL --	
+-- (2 dấu gạch liên tiếp): Comment trên một dòng và chỉ có tác dụng trên 1 dòng DungNA29
+/*..Comment trên một khối.. */ 
+        
+/*
+	CÂU LỆNH 1.2: CREATE DATABASE
+	ĐỊNH NGHĨA: DÙNG ĐỂ TẠO 1 DATABASE TRONG CSDL
+	CÔNG THỨC: 
+	CREATE DATABASE database_name;
+*/
+USE Dungna_FPTShopV2;--Chỉ định database nào sẽ làm việc
+
+CREATE DATABASE Dungna_FPTShopV2;
+
+
+/*
+	CÂU LỆNH 1.3: DROP DATABASE
+	ĐỊNH NGHĨA: XÓA DATABASE TRONG CSDL
+	CÔNG THỨC: 
+	DROP DATABASE database_name;
+*/
+DROP DATABASE Dungna_FPTShopV2;
+/*
+	CÂU LỆNH 1.4: CREATE TABLE
+	ĐỊNH NGHĨA: TẠO BẢNG TRONG CSDL
+	CÔNG THỨC: 
+
+	CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+	);
+*/
+
+CREATE TABLE DongSanPham(
+ID INT PRIMARY KEY IDENTITY, 
+MaDSP VARCHAR(10) UNIQUE,
+TenDSP NVARCHAR(50) DEFAULT NULL,
+);
+/*
+	CÂU LỆNH 1.5: DROP TABLE
+	ĐỊNH NGHĨA: XÓA BẢNG TRONG CSDL
+	CÔNG THỨC: 
+	DROP TABLE table_name;
+*/
+DROP TABLE DongSanPham;
+/*
+	CÂU LỆNH 1.6: ALTER TABLE
+	ĐỊNH NGHĨA:
+		- Câu lệnh ALTER TABLE được sử dụng để thêm, xóa hoặc sửa đổi các cột trong bảng hiện có.
+		- Câu lệnh ALTER TABLE cũng được sử dụng để thêm và bỏ các ràng buộc khác nhau trên một bảng hiện có.
+	CÔNG THỨC: 
+		- Thêm cột:
+		ALTER TABLE table_name
+		ADD column_name datatype;
+
+		- Xóa cột:
+		ALTER TABLE table_name
+		DROP COLUMN column_name;
+*/
+ALTER TABLE DongSanPham
+		ADD ColTest int;
+
+ALTER TABLE DongSanPham
+		DROP COLUMN ColTest;
